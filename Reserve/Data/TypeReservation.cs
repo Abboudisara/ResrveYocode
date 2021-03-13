2209db97ed067bd648809ce2389c3ce8fee04778
Abboudisara <57992532+Abboudisara@users.noreply.github.com>
2021-03-13 09:38:51 +0100

    solveprobpush
    Register-login-typeR-R

---
+++ Reserve/Data/TypeReservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reserve.Data
+{
+    public class TypeReservation
+    {
+        [Key]
+
+        public int id_type { get; set; }
+        [Required(ErrorMessage = "Entrer Nom de Reservation")]
+        [Display(Name = "Reservation")]
+        public string Nom { get; set; }
+        [Required(ErrorMessage = "Entrer Nombre acces ")]
+        [Display(Name = "Nombre")]
+        public int NombreApp { get; set; }
+    }
+}
---
+++ Reserve/Data/reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reserve.Data
+{
+    public class reservation
+    {
+        [Key]
+        public int id_reservation { get; set; }
+
+        [Required(ErrorMessage = "Entrer Date")]
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        public string date { get; set; }
+        [Required(ErrorMessage = "Svp choisir un Type Reservation ")]
+        public int typeReservationid_type { get; set; }
+
+        [ForeignKey(nameof(typeReservationid_type))]
+        public TypeReservation typeReservation { get; set; }
+
+        [ForeignKey("User_id")]
+        public Utitlisateur user { get; set; }
+        public string User_id { get; set; }
+    }
+}
